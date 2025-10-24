369557b53afe26557c0cccc03aa99687058958ca
MsManish09 <msmanish680@gmail.com>
2025-10-24 18:43:23 +0530

    BrowseBook component done, need to add link feature

--- src/components/NewBrowseBooks.jsx
+++ src/components/NewBrowseBooks.jsx
@@ -12,11 +12,11 @@ function NewBrowseBooks(){
     return(
         <div>
             <Header />
-            <div className="  p-2 m-2 flex lg:flex-row flex-col justify-evenly  ">
+            <div className="  p-2 m-2 flex lg:flex-row flex-col justify-evenly gap-4  ">
 
                 {/* popular books section */}
                 {/*  */}
-                <section className=" bg-[rgba(255,255,255,0.5)]  rounded-2xl p-2 lg:w-[20%] h-80% flex flex-col  flex-wrap justify-center items-center gap-4 sm:w-[95%]  ">
+                <section className=" bg-[rgba(255,255,255,0.5)]  rounded-2xl p-2 lg:w-[22%] h-fit flex flex-col  flex-wrap justify-start items-center gap-4 sm:w-[95%]  ">
                     <h3 className=" text-2xl font-semibold underline text-yellow-300 " >Popular Books</h3>
                     {
                         popularBook.map((book, index)=> <BookCard book={book} key={index} /> )
@@ -25,8 +25,19 @@ function NewBrowseBooks(){
                 </section>
 
                 {/* browse bbooks section */}
-                <section className="bg-blue-300 p-4 w-[65%] h-50px flex flex-col items-center  ">
-                    <h3 className=" text-2xl font-semibold underline " >Browse Books</h3>
+                <section className="bg-[rgba(255,255,255,0.5)] rounded-[15px] p-4 h-50px flex flex-col gap-3 items-center lg:w-[70%] mt-4  ">
+                    <h3 className=" text-2xl font-semibold text-blue-700  underline " >Browse Books</h3>
+                    
+                    <div className="flex flex-wrap gap-4 justify-evenly" >
+                        {
+                            booksData.map((book)=>{
+                                return(
+                                    <BookCard book={book} />
+                                )
+                            })
+                        }
+                    </div>
+
                 </section>
             </div>
         </div>
